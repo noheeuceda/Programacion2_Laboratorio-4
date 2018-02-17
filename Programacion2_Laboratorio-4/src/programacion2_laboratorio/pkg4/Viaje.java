acdba07b5f300bfefb97135bdb2f7a5990e5f5c8
noheeuceda <neuceda@unah.hn>
2018-02-16 18:04:36 -0600

    errores

---
+++ Programacion2_Laboratorio-4/src/programacion2_laboratorio/pkg4/Viaje.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programacion2_laboratorio.pkg4;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author euced
+ */
+public class Viaje {
+    ArrayList<MedioTransporte> trans = new ArrayList();
+    ArrayList<Primates> primate = new ArrayList();
+    
+    
+}
